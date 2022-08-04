74c0b4246adac0bd8a19cf0a73d5b89d79298eaa
Camden McLeod <ccmcleod05@gmail.com>
2022-08-04 13:57:12 -0500

    Favicon is working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head'
 import { ParallaxProvider } from 'react-scroll-parallax';
 import '../styles/main.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ParallaxProvider>
-      <Component {...pageProps} />
-    </ParallaxProvider>
+    <>
+      <Head> 
+        <link rel="shortcut icon" href="/favicon.ico"/>
+      </Head>
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
+    </>
   );
 };
 

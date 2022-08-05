91bb92be459a308c406b0135153eb27b01652bdb
Kevin G <ge.kevin.a@gmail.com>
2022-08-04 20:58:28 -0500

    Fix type errors and delete comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,40 +5,11 @@ import { LandingPage } from '../ui/landing/LandingPage';
 import { ProjectDisplay } from '../ui/projects/ProjectDisplay';
 
 const HomePage: NextPage = () => {
-  // return (
-  //   <ProjectDisplay
-  //     projects={[
-  //       {
-  //         name: 'Greenway',
-  //         description:
-  //           'A fast and interactive way to learn about the manufacturing of your belongings, and what the you can do to reduce your footprint.',
-  //         imgSrc: 'https://crhsidea.org/static/images/Greenway.jpg',
-  //         competitionName: 'Silicon Valley Hacks',
-  //         date: new Date(),
-  //         awardName: 'Best Climate Change Hack',
-  //         won: true,
-  //       },
-  //       {
-  //         name: 'Bot Dilemma',
-  //         description:
-  //           'A fast and interactive way to learn about the manufacturing of your belongings, and what the you can do to reduce your footprint.',
-  //         imgSrc: 'https://crhsidea.org/static/images/Greenway.jpg',
-  //         competitionName: 'Something Hacks',
-  //         date: new Date(),
-  //         awardName: 'Best Hack for Something',
-  //         won: true,
-  //       },
-  //     ]}
-  //   />
-  // );
-
-  const isPhone = useMediaQuery('(max-width: 768px)');
-
-  // return <LandingPage />;
+  const disableParallax = useMediaQuery('(max-width: 768px)');
 
   return (
     <div>
-      {isPhone ? (
+      {disableParallax ? (
         <LandingPage />
       ) : (
         <Parallax speed={-50}>
--- src/ui/projects/ProjectBanner.tsx
+++ src/ui/projects/ProjectBanner.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 interface ProjectBannerProps {
   competitionName: string;
+  competitionURL: string;
   won?: boolean;
 }
 

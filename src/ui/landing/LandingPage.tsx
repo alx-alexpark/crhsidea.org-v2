80f931e58f265fd328bfa79fa2800ceb5d1652f9
CM <74691129+ccmcleod05@users.noreply.github.com>
2022-08-10 22:52:15 -0500

    Update LandingPage.tsx

--- src/ui/landing/LandingPage.tsx
+++ src/ui/landing/LandingPage.tsx
@@ -41,7 +41,7 @@ export const LandingPage: FC = () => {
           <p className='lp-p'>Start your CS journey today</p>
 
           <div className='media-wrap'>
-            <a href='https://www.remind.com/join/crhside' target='_blank' rel='noreferrer'>
+            <a href='https://www.remind.com/join/crhsidea23' target='_blank' rel='noreferrer'>
               <RemindLogoIcon className='media-svg' />
             </a>
             <a href='https://github.com/crhsidea' target='_blank' rel='noreferrer'>
